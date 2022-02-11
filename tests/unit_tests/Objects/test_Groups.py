d3230d8f4619d4084cb7387b5acbbde275ddf0fd
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2022-02-11 11:20:18 +0100

    Add more tests
    Forgot to add the tests for sorting Collections

--- tests/unit_tests/Objects/test_Groups.py
+++ tests/unit_tests/Objects/test_Groups.py
@@ -477,3 +477,26 @@ def test_baseCollection_set_index_based(cls):
     for item in obj:
         assert obj._borg.map.convert_id_to_key(item) in edges
     assert obj._borg.map.convert_id_to_key(p4) not in edges
+
+
+@pytest.mark.parametrize('cls', class_constructors)
+def test_baseCollection_sort(cls):
+    name = 'test'
+    v = [1, 4, 3, 2, 5]
+    expected = [1, 2, 3, 4, 5]
+    d = cls(name, *[Parameter(f'p{i}', v[i]) for i in range(len(v))])
+    d.sort(lambda x: x.raw_value)
+    for i, item in enumerate(d):
+        assert item.value == expected[i]
+
+
+@pytest.mark.parametrize('cls', class_constructors)
+def test_baseCollection_sort_reverse(cls):
+    name = 'test'
+    v = [1, 4, 3, 2, 5]
+    expected = [1, 2, 3, 4, 5]
+    expected.reverse()
+    d = cls(name, *[Parameter(f'p{i}', v[i]) for i in range(len(v))])
+    d.sort(lambda x: x.raw_value, reverse=True)
+    for i, item in enumerate(d):
+        assert item.raw_value == expected[i]
