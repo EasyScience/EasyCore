f34b879423e5fb9517e14575c0443dd009700206
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2022-11-24 10:07:46 +0100

    Clean up Graph and add exceptions

--- easyCore/Objects/Graph.py
+++ easyCore/Objects/Graph.py
@@ -7,6 +7,7 @@ from __future__ import annotations
 __author__ = "github.com/wardsimon"
 __version__ = "0.1.0"
 
+import warnings
 from typing import List, Union, TYPE_CHECKING, Optional, Tuple
 from weakref import WeakKeyDictionary, ref
 from collections import defaultdict
@@ -31,23 +32,41 @@ uniqueidmap = UniqueIdMap()
 
 class Graph:
     def __init__(self):
-        # self._store = WeakValueDictionary()
-        # self.__graph_dict = {}
-        self._G = nx.DiGraph()
+        self._graphs = {
+            "base": nx.DiGraph(),
+        }
         self.__known_types = {"argument", "created", "created_internal", "returned"}
 
+    def create_synced_graph(self, graph_name: str, graph_type=None):
+        if graph_type is None:
+            graph_type = nx.DiGraph
+        self._graphs[graph_name] = graph_type()
+        self._graphs[graph_name].add_nodes_from(self._G.nodes(data=True))
+
+    def remove_synced_graph(self, graph_name: str):
+        if graph_name in self._graphs and graph_name != "base":
+            self._graphs.pop(graph_name)
+
+    def synced_graph_names(self) -> List[str]:
+        return list(self._graphs.keys())
+
+    @property
+    def _G(self):
+        return self._graphs["base"]
+
     def reset_graph(self):
-        # self._store = WeakValueDictionary()
-        # self.__graph_dict = {}
-        self._G = nx.DiGraph()
+        for key in self._graphs.keys():
+            self._graphs[key] = nx.DiGraph()
 
     def nodes(self) -> List[str]:
         """returns the nodes of a graph"""
         return list(self._G.nodes)
 
-    def edges(self) -> List[Tuple[str, str]]:
+    def edges(self, graph: str = "base") -> List[Tuple[str, str]]:
         """returns the edges of a graph"""
-        return list(self._G.edges)
+        if graph not in self._graphs:
+            raise ValueError(f"Graph {graph} not found")
+        return list(self._graphs[graph].edges)
 
     def _query_graph_types(self, created_type):
         return [n for n, d in self._G.nodes(data=True) if created_type in d["type"]]
@@ -69,22 +88,25 @@ class Graph:
         return self._query_graph_types("returned")
 
     def get_item_by_key(self, item_id: str) -> Optional[BV]:
-        item_id = str(item_id)
+        if item_id not in self._G.nodes:
+            item_id = str(item_id)
+        if item_id not in self._G.nodes:
+            raise ValueError(f"Item {item_id} not found")
         return self._G.nodes.get(item_id, {"object": lambda: None})["object"]()
 
     def is_known(self, node: BV) -> bool:
-        return str(self.convert_id(node).int) in self._G.nodes
+        return self.convert_id(node, as_string=True) in self._G.nodes
 
     def find_type(self, node: BV) -> List[str]:
-        item_id = str(self.convert_id(node).int)
+        item_id = self.convert_id(node, as_string=True)
         return self._G.nodes.get(item_id, {"type": []})["type"]
 
     def reset_type(self, node: BV, default_type: str):
-        item_id = str(self.convert_id(node).int)
+        item_id = self.convert_id(node, as_string=True)
         self._G.nodes[item_id]["type"] = [default_type]
 
     def change_type(self, node: BV, new_type: str):
-        item_id = str(self.convert_id(node).int)
+        item_id = self.convert_id(node, as_string=True)
         if (
             new_type in self.__known_types
             and new_type not in self._G.nodes[item_id]["type"]
@@ -92,56 +114,91 @@ class Graph:
             self._G.nodes[item_id]["type"].append(new_type)
 
     def add_node(self, node: BV, obj_type: str = None):
-        oid = self.convert_id(node).int
-        self._G.add_node(str(oid), object=ref(node), type=[obj_type])
+        oid = self.convert_id(node, as_string=True)
+        for graph_key in self._graphs.keys():
+            try:
+                object_ref = ref(node)
+            except TypeError:
+                object_ref = lambda: node
+            self._graphs[graph_key].add_node(oid, object=object_ref, type=[obj_type])
 
-    def add_edge(self, start_obj: BV, end_obj: BV):
-        node1 = str(self.convert_id(start_obj).int)
-        node2 = str(self.convert_id(end_obj).int)
-        weight = 0.5
+    def add_edge(
+        self,
+        start_obj: BV,
+        end_obj: BV,
+        weight=None,
+        graph: str = "base",
+        additonal_data: dict = None,
+    ):
+        if graph not in self._graphs:
+            raise ValueError(f"Graph {graph} not found")
+        node1 = self.convert_id(start_obj, as_string=True)
+        node2 = self.convert_id(end_obj, as_string=True)
+        this_weight = 0.5
         if hasattr(end_obj, "value"):
-            weight = 5
-        self._G.add_weighted_edges_from([(node1, node2, weight)])
+            this_weight = 5
+        if weight is None:
+            weight = this_weight
+        if additonal_data is None:
+            additonal_data = {}
+        self._graphs[graph].add_weighted_edges_from(
+            [(node1, node2, weight)], **additonal_data
+        )
 
-    def get_edges(self, start_obj: BV) -> List[Tuple[str, str]]:
-        node1 = str(self.convert_id(start_obj).int)
-        return list(self._G.edges(node1))
+    def get_edges(self, start_obj: BV, graph: str = "base") -> List[Tuple[str, str]]:
+        if graph not in self._graphs:
+            raise ValueError(f"Graph {graph} not found")
+        node1 = self.convert_id(start_obj, as_string=True)
+        return list(self._graphs[graph].edges(node1))
 
-    def prune_node_from_edge(self, parent_obj: BV, child_obj: BV):
-        vertex1 = str(self.convert_id(parent_obj).int)
+    def prune_node_from_edge(self, parent_obj: BV, child_obj: BV, graph: str = "base"):
+        if graph not in self._graphs:
+            raise ValueError(f"Graph {graph} not found")
+        vertex1 = self.convert_id(parent_obj, as_string=True)
         if child_obj is None:
             return
-        vertex2 = str(self.convert_id(child_obj).int)
+        vertex2 = self.convert_id(child_obj, as_string=True)
         if (vertex1, vertex2) in self._G.edges:
-            self._G.remove_edge(vertex1, vertex2)
+            self._graphs[graph].remove_edge(vertex1, vertex2)
 
     def prune(self, key: int):
-        key = str(key)
-        if key in self._G.nodes:
-            self._G.remove_node(key)
+        if key not in self._G.nodes:
+            key = str(key)
+            if key not in self._G.nodes:
+                raise ValueError(f"Item {key} not found")
+        for graph_key in self._graphs.keys():
+            if key in self._graphs[graph_key].nodes:
+                self._graphs[graph_key].remove_node(key)
 
-    def find_isolated_nodes(self) -> List[int]:
+    def find_isolated_nodes(self, graph: str = "base") -> List[int]:
         """returns a list of isolated nodes."""
-        return list(nx.isolates(self._G))
+        if graph not in self._graphs:
+            raise ValueError(f"Graph {graph} not found")
+        return list(nx.isolates(self._graphs[graph]))
 
     def find_path(
         self,
         start_obj: Union[BV, int],
         end_obj: Union[BV, int],
         path: Optional[List[str]] = None,
+        graph: str = "base",
     ) -> List[str]:
+        if graph not in self._graphs:
+            raise ValueError(f"Graph {graph} not found")
         """find a path from start_vertex to end_vertex
         in graph"""
         if path is None:
             path = []
         try:
-            start_vertex = self.convert_id(start_obj).int
-            end_vertex = self.convert_id(end_obj).int
+            start_vertex = self.convert_id(start_obj, as_string=True)
+            end_vertex = self.convert_id(end_obj, as_string=True)
         except TypeError:
             start_vertex = start_obj
             end_vertex = end_obj
         try:
-            return path + nx.shortest_path(self._G, str(start_vertex), str(end_vertex))
+            return path + nx.shortest_path(
+                self._graphs[graph], start_vertex, end_vertex
+            )
         except nx.exception.NetworkXNoPath:
             return path
 
@@ -150,26 +207,32 @@ class Graph:
         start_obj: Union[BV, int],
         end_obj: Union[BV, int],
         path: Optional[List[str]] = None,
+        graph: str = "base",
     ) -> List[List[str]]:
         """find all paths from start_vertex to
         end_vertex in graph"""
+        if graph not in self._graphs:
+            raise ValueError(f"Graph {graph} not found")
         if path is None:
             path = []
         try:
-            start_vertex = self.convert_id(start_obj).int
-            end_vertex = self.convert_id(end_obj).int
+            start_vertex = self.convert_id(start_obj, as_string=True)
+            end_vertex = self.convert_id(end_obj, as_string=True)
         except TypeError:
             start_vertex = start_obj
             end_vertex = end_obj
         try:
             return path + nx.all_shortest_paths(
-                self._G, str(start_vertex), str(end_vertex)
+                self._graphs[graph], start_vertex, end_vertex
             )
         except nx.exception.NetworkXNoPath:
             return path
 
     def reverse_route(
-        self, end_obj: Union[BV, int], start_obj: Optional[Union[BV, int]] = None
+        self,
+        end_obj: Union[BV, int],
+        start_obj: Optional[Union[BV, int]] = None,
+        graph: str = "base",
     ) -> List[str]:
         """
         In this case we have an object and want to know the connections to get to another in reverse.
@@ -181,66 +244,32 @@ class Graph:
         :return:
         :rtype:
         """
+        if graph not in self._graphs:
+            raise ValueError(f"Graph {graph} not found")
         if start_obj is None:
-            start_node = str(self.convert_id(end_obj).int)
+            start_node = self.convert_id(end_obj, as_string=True)
             nodes = list(
-                nx.dag_longest_path(nx.dfs_tree(self._G.reverse(), source=start_node))
+                nx.dag_longest_path(
+                    nx.dfs_tree(self._graphs[graph].reverse(), source=start_node)
+                )
             )
             return nodes
         else:
-            path = self.find_path(start_obj, end_obj)
+            path = self.find_path(start_obj, end_obj, graph=graph)
             path.reverse()
             return path
-        # first_decendent = nx.descendants(reversed_graph, start_node)
-        #
-        # path_length = sys.maxsize
-        # optimum_path = []
-        # if start_obj is None:
-        #     # We now have to find where to begin.....
-        #     for possible_start, nodes in self.__graph_dict.items():
-        #         if end_vertex in nodes:
-        #             temp_path = self.find_path(possible_start, end_vertex)
-        #             if len(temp_path) < path_length:
-        #                 path_length = len(temp_path)
-        #                 optimum_path = temp_path
-        # else:
-        #     optimum_path = self.find_path(start_obj, end_obj)
-        # optimum_path.reverse()
-        # return optimum_path
-
-    # def is_connected(self, nodes_encountered=None, start_vertex=None) -> bool:
-    #     """determines if the graph is connected"""
-    #     if nodes_encountered is None:
-    #         nodes_encountered = set()
-    #     graph = self.__graph_dict
-    #     nodes = list(graph.keys())
-    #     if not start_vertex:
-    #         # chose a vertex from graph as a starting point
-    #         start_vertex = nodes[0]
-    #     nodes_encountered.add(start_vertex)
-    #     if len(nodes_encountered) != len(nodes):
-    #         for vertex in graph[start_vertex]:
-    #             if vertex not in nodes_encountered and self.is_connected(
-    #                 nodes_encountered, vertex
-    #             ):
-    #                 return True
-    #     else:
-    #         return True
-    #     return False
-
-    # def _nested_get(self, obj_type: str) -> List[int]:
-    #     """Access a nested object in root by key sequence."""
-    #     extracted_list = []
-    #     for key, item in self.__graph_dict.items():
-    #         if obj_type in item.type:
-    #             extracted_list.append(key)
-    #     return extracted_list
 
     @staticmethod
-    def convert_id(input_value) -> UUID:
+    def convert_id(input_value, as_string=False) -> UUID:
         """Sometimes we're dopy and"""
-        if not validate_id(input_value):
-            input_value = unique_id(input_value)
+        try:
+            if not validate_id(input_value):
+                input_value = unique_id(input_value)
+        except TypeError:
+            warnings.warn(f"Unable to convert {input_value} to UUID")
+            return input_value
+        if as_string:
+            input_value = str(input_value.int)
         return input_value
 
     @staticmethod
@@ -262,7 +291,7 @@ class Graph:
             raise EnvironmentError(
                 "Holoviews needs to be installed in order to use `plot`"
             )
-        start_node_id = str(self.convert_id(node).int)
+        start_node_id = self.convert_id(node, as_string=True)
         to_add = {
             "name": "name",
             "color": "_color",
